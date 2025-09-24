5a8a70e126df6e78696253baed61c4547a1878ba
Bibidh Subedi <080bct021.bibidh@pcampus.edu.np>
2025-09-24 16:06:50 +0545

    Added the detele option for noteboxes

--- NoteWiki/Controllers/HomeController.cs
+++ NoteWiki/Controllers/HomeController.cs
@@ -1,22 +1,27 @@
 ﻿using System.Security.Claims;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
 using NoteWiki.Data;
 using NoteWiki.Models;
+using static Azure.Core.HttpHeader;
 
 namespace NoteWiki.Controllers
 {
     public class HomeController : Controller
     {
         private readonly AppDbContext _context;
+        private readonly IMongoCollection<NoteContentModel> _MongoContext;
         protected Guid GetUserGuid(string email, AppDbContext context)
         {
             //var email = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
             return _context.Users.FirstOrDefault(c => c.Email == email)?.UserID??throw new Exception("User not found");
         }
-        public HomeController(AppDbContext context)
+        public HomeController(AppDbContext context, MongoContext mongoContext)
         {
             _context = context;
+            _MongoContext = mongoContext.Database?.GetCollection<NoteContentModel>("notes")
+            ?? throw new Exception("Could not connect to MongoDB collection.");
         }
         public IActionResult Index()
         {
@@ -50,6 +55,32 @@ namespace NoteWiki.Controllers
             return RedirectToAction("Index", "Home");
         }
 
+        public IActionResult Options(Guid id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+
+        public IActionResult RemoveBox(Guid id)
+        {
+            var box = _context.NoteBoxes.FirstOrDefault(c => c.NoteBoxGuid == id);
+            var notesMetadata = _context.NoteMetadata.Where(n => n.NoteBoxGuid == id).ToList();
+            if (box == null)
+            {
+                return NotFound($"Box with ID {id} not found.");
+            }
+            foreach (var note in notesMetadata)
+            {
+                _MongoContext.DeleteOne(m => m.NoteGuid == id);
+            }
+
+
+            _context.NoteMetadata.RemoveRange(notesMetadata);
+            _context.NoteBoxes.Remove(box);
+            _context.SaveChanges();
+
+            return Content($"Removed all stuff of {id}");
+        }
         // Seed
         public async Task<IActionResult> AddNoteBox()
         {
